465ed2b62ec3db454cb5dff4685a1a9dc8117f51
Paul Quinnell <paulquinnell@gmail.com>
2022-11-06 14:23:18 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Paul Quinnell on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
